5f4dd772df7c6615f5d340e289cc210cdaba68e4
Lakshmichanna <lakshmichanna23@gmail.com>
2024-11-01 17:22:39 +0530

    Updated expense creation

---
+++ cypress/e2e/Expense.cy.js
@@ -0,0 +1,34 @@
+import Loginpage from './Pages/Loginpage'
+import Receiptpage from './Pages/Receiptpage';
+import Expensepage from './Pages/Expensepage'
+
+
+
+describe('Trip Approval', () => {
+
+  const lp = new Loginpage()
+  const rp = new Receiptpage();
+  const ep = new Expensepage()
+
+  it('managerapproval',()=>{
+
+    cy.fixture('Login').then((approval) => {
+
+        lp.launch()
+        lp.login(approval.email, approval.password)
+        lp.popupoverride()
+        ep.addexpense()
+        cy.fixture('Expense').then((rec) => {
+          
+        ep.basicreceipt(rec.expensetype,rec.description,rec.country,rec.cost,rec.paymenttype,rec.vendor)
+        cy.fixture('Templatefields').then((fields)=>{
+          ep.templatefields(fields.vendor,fields.location,fields.items)
+        })
+        ep.recepitlist()
+    })
+  
+  })
+
+  })
+
+})
